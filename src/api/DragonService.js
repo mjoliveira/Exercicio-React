591fb5ecbcc4731d3d61bade6f560373d56bf017
mjoliveira <mayara.jesus@acad.pucrs.br>
2020-03-26 11:45:23 -0300

    Refatora DragonService

--- src/api/DragonService.js
+++ src/api/DragonService.js
@@ -1,46 +1,32 @@
 import Axios from "axios";
 
+const baseUrl = 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api';
+
 export default class DragonService {
 
   static async getById(id) {
-    const { data } = await Axios.get('http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon/' + id);
+    const { data } = await Axios.get(`${baseUrl}/v1/dragon/${id}`);
     return data;
   }
 
   static async list() {
-    const { data } = await Axios.get('http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon');
+    const { data } = await Axios.get(`${baseUrl}/v1/dragon`);
     return data;
   }
 
   static async create({ name, type, histories }) {
-    await Axios.post('http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon', {
-      name: name,
-      type: type,
-      histories: histories,
-    }).then(res => {
-      console.log(res);
-      console.log(res.data);
-    });
+    const { data } = await Axios.post(`${baseUrl}/v1/dragon`, { name, type, histories });
+    return data;
   }
 
   static async delete(id) {
-    await Axios.delete('http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon/' + id)
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      });
+    const { data } = await Axios.delete(`${baseUrl}/v1/dragon/${id}`);
+    return data;
   }
 
   static async update({ id, name, type, histories }) {
-    await Axios.put('http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon/' + id, {
-      name: name,
-      type: type,
-      histories: histories,
-    })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      });
+    const { data } = await Axios.put(`${baseUrl}/v1/dragon/${id}`, { name, type, histories });
+    return data;
   }
 
 }
