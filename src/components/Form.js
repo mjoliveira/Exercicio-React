6a5de74fea3138c3b8cd12eea81adde16a610337
mjoliveira <mayara.jesus@acad.pucrs.br>
2020-03-26 11:43:23 -0300

    Conclui componente form e edição de dragão

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import DragonService from "../api/DragonService";
 import { useFormik } from "formik";
 import * as yup from 'yup';
 import { navigate } from '@reach/router';
 
-function Form(data) {
-  const id = data.id;
-  const [dragon, setDragon] = useState({
-    name: "",
-    type: "",
-    histories: "",
-  });
-
-  console.log("Id:   " + id);
-
-  const initialValues = (async () => {
-    if (id === null) {
-      return dragon;
-    }
-
-    await DragonService.getById(id)
-      .then((data) => { return data; })
-      .then((dataDragon) => { setDragon(dataDragon) });
-
-    console.log("Form: " + JSON.stringify(dragon));
-    console.log("Nome:   " + dragon.name);
-    return dragon;
-  });
-
-  const { getFieldProps, touched, errors, isValid, handleSubmit } = useFormik({
-    initialValues: { initialValues },
+export default function Form({ id }) {
+  const { setFieldValue, getFieldProps, touched, errors, isValid, handleSubmit } = useFormik({
+    initialValues: {
+      name: "",
+      type: "",
+      histories: "",
+    },
     validationSchema: yup.object({
       name: yup
         .string()
@@ -42,77 +23,82 @@ function Form(data) {
         .string()
         .required("O preencimento da história é obrigatória."),
     }),
-    onSubmit: values => {
-      if (id === null) {
-        const registerData = {
-          name: values.name,
-          type: values.type,
-          histories: values.histories
-        };
+    onSubmit: async ({ name, type, histories }) => {
+      try {
+        const registerData = { name, type, histories};
 
-        DragonService.create({ ...registerData });
+        if (id) {
+          registerData.id = id;
+          await DragonService.update(registerData);
+        } else {
+          await DragonService.create(registerData);
+        }
 
-      } else {
-        const registerData = {
-          id: id,
-          name: values.name,
-          type: values.type,
-          histories: values.histories
-        };
+        navigate("/");
 
-        DragonService.update({ ...registerData });
+      } catch (error) {
+        console.log('error ==>', error);
+        alert("Não foi possível salvar o dragão. Tente novamente!");
       }
-      navigate("/");
     }
   });
 
+  useEffect(() => {
+    (async () => {
+      if (id) {
+        const response = await DragonService.getById(id);
+        setFieldValue('name', response.name);
+        setFieldValue('type', response.type);
+        setFieldValue('histories', response.histories);
+      }
+    })();
+  }, [id, setFieldValue]);
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <table className="table-create">
-          <thead>
-            <tr>
-              {id === null ? <th colSpan="2"><h2 className="subtitle" >Crie um dragão!</h2></th> : <th colSpan="2"><h2 className="subtitle" >Editar um dragão!</h2></th>}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th><label htmlFor="name">Nome</label></th>
-              <th>
-                <input id="name" type="text" value={dragon.name} autocomplite="off" {...getFieldProps("name")} />
-                {touched.name && errors.name ? (
-                  <small>{errors.name}</small>
-                ) : null}
-              </th>
-            </tr>
-            <tr>
-              <th><label htmlFor="type">Tipo</label></th>
-              <th>
-                <input id="type" type="text" autocomplite="off" {...getFieldProps("type")} />
-                {touched.type && errors.type ? (
-                  <small>{errors.type}</small>
-                ) : null}
-              </th>
-            </tr>
-            <tr>
-              <th><label htmlFor="histories">História</label></th>
-              <th>
-                <textarea id="histories" rows="4" cols="20" name="histories" {...getFieldProps("histories")} />
-                {touched.histories && errors.histories ? (
-                  <small>{errors.histories}</small>
-                ) : null}
-              </th>
-            </tr>
-            <tr>
-              <th colSpan="2">
-                <button type="submit" disabled={!isValid}>Salvar</button>
-              </th>
-            </tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <table className="table-create">
+        <thead>
+          <tr>
+            <th colSpan="2">
+              <h2 className="subtitle">{id ? "Editar" : "Crie"} um dragão!</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th><label htmlFor="name">Nome</label></th>
+            <th>
+              <input id="name" type="text" {...getFieldProps("name")} />
+              {touched.name && errors.name ? (
+                <small>{errors.name}</small>
+              ) : null}
+            </th>
+          </tr>
+          <tr>
+            <th><label htmlFor="type">Tipo</label></th>
+            <th>
+              <input id="type" type="text" {...getFieldProps("type")} />
+              {touched.type && errors.type ? (
+                <small>{errors.type}</small>
+              ) : null}
+            </th>
+          </tr>
+          <tr>
+            <th><label htmlFor="histories">História</label></th>
+            <th>
+              <textarea id="histories" rows="4" cols="20" {...getFieldProps("histories")} />
+              {touched.histories && errors.histories ? (
+                <small>{errors.histories}</small>
+              ) : null}
+            </th>
+          </tr>
+          <tr>
+            <th colSpan="2">
+              <button type="submit" disabled={!isValid}>Salvar</button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </form>
   );
 }
-
-export default Form;
